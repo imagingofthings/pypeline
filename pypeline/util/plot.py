827f8544dd5d91721b888703477e9702bc8f19f6
Sepand KASHANI <kashani.sepand@gmail.com>
2019-06-16 13:10:13 +0200

    [Code/Doc] Remove `util.plot` methods.

--- pypeline/util/plot.py
+++ pypeline/util/plot.py
@@ -10,55 +10,11 @@
 
 import pathlib
 
-import matplotlib.axes as axes
-import matplotlib.cm as cm
 import matplotlib.colors as col
-import mpl_toolkits.axes_grid1 as ax_grid
 import pandas as pd
 import pkg_resources as pkg
 
 import pypeline.util.argcheck as chk
-
-
-@chk.check(dict(scm=chk.is_instance(cm.ScalarMappable), ax=chk.is_instance(axes.Axes)))
-def colorbar(scm, ax):
-    """
-    Attach colorbar to side of a plot.
-
-    Parameters
-    ----------
-    scm : :py:class:`~matplotlib.cm.ScalarMappable`
-        Intensity scale.
-    ax : :py:class:`~matplotlib.axes.Axes`
-        Plot next to which the colorbar is placed.
-
-    Returns
-    -------
-    colorbar : :py:class:`~matplotlib.colorbar.Colorbar`
-
-    Examples
-    --------
-    .. doctest::
-
-       import numpy as np
-       import matplotlib.pyplot as plt
-       from pypeline.util.plot import colorbar
-
-       x, y = np.ogrid[-1:1:100j, -1:1:100j]
-
-       fig, ax = plt.subplots()
-       im = ax.imshow(x + y, cmap='jet')
-       cb = colorbar(im, ax)
-
-       fig.show()
-
-    .. image:: _img/colorbar_example.png
-    """
-    fig = ax.get_figure()
-    divider = ax_grid.make_axes_locatable(ax)
-    ax_colorbar = divider.append_axes("right", size="5%", pad=0.05, axes_class=axes.Axes)
-    colorbar = fig.colorbar(scm, cax=ax_colorbar)
-    return colorbar
 
 
 @chk.check(dict(name=chk.is_instance(str), N=chk.allow_None(chk.is_integer)))
@@ -75,7 +31,8 @@ def cmap(name, N=None):
     N : int, optional
         Number of color levels. (Default: all).
 
-        If `N` is smaller than the number of levels available in the colormap, then the last `N` colors will be used.
+        If `N` is smaller than the number of levels available in the colormap, then the last `N`
+        colors will be used.
 
     Returns
     -------
@@ -87,6 +44,7 @@ def cmap(name, N=None):
 
        import numpy as np
        import matplotlib.pyplot as plt
+
        from pypeline.util.plot import cmap
 
        x, y = np.ogrid[-1:1:100j, -1:1:100j]
