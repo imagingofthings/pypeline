ccf15e24ff6a4c153d44ca13cee4668c939a2ad7
etolley <eetolley@gmail.com>
2020-09-11 11:33:32 +0200

    added setup/install instructions

--- README.rst
+++ README.rst
@@ -10,7 +10,32 @@ Pypeline
 *Pypeline* is a `GPLv3 <https://www.gnu.org/licenses/gpl-3.0.en.html>`_ signal processing toolkit to
 design and deploy holistic imaging pipelines.
 
+This is a fork used for benchmarking and testing the code. 
+This repository is also mirrored at:
+https://c4science.ch/diffusion/10745/repository/master/
 
 Installation
 ------------
-See ``doc/general/install.rst``
+For general installation, see ``doc/general/install.rst``
+
+For installation on EPFL cluster, first you must install condo. You can run 
+```
+bash Miniconda3-latest-Linux-x86_64.sh 
+```
+And respond to the interactive instructions. Be sure to install conda into your home directory. 
+
+Make a new directory which will contain pypeline and all of its packages. cd into this dir, then run:
+```
+git clone https://github.com/etolley/pypeline.git
+cp pypeline/install.sh .
+```
+Afterwards, simply run:
+```
+source install.sh
+```
+For any future setup, simply run
+```
+export PATH=$HOME/miniconda3/bin/:$PATH
+cd pypeline/
+source pypeline.sh --no_shell
+```
---
+++ install.sh
@@ -0,0 +1,51 @@
+
+#### Install conda
+
+## you must respond to the interactive instructions following this command
+## be sure to install conda into your home directory
+#bash Miniconda3-latest-Linux-x86_64.sh 
+
+export PATH=$HOME/miniconda3/bin/:$PATH
+conda update -n base -c defaults conda
+
+#### Install bluebild
+
+git clone https://github.com/etolley/pypeline.git
+cd pypeline/
+
+
+##  create conda environment
+conda create --name=pypeline       \
+             --channel=defaults    \
+             --channel=conda-forge \
+             --file=conda_requirements.txt
+source pypeline.sh --no_shell
+
+cd ..
+
+git clone https://github.com/imagingofthings/pyFFS.git
+cd pyFFS/
+git checkout v1.0
+mkdir install
+python3 setup.py develop --user
+
+cd ..
+
+git clone https://github.com/imagingofthings/ImoT_tools.git
+cd ImoT_tools/
+git checkout v1.0
+python3 setup.py develop --user
+
+cd ..
+
+cd pypeline/
+# you may need to modify this path for your own directory structure
+export PYTHONPATH="$PYTHONPATH:$HOME/bluebild/pyFFS/install:$HOME/bluebild/ImoT_tools/install"
+
+python3 setup.py develop --user
+
+# you may need to modify this path for your own directory structure
+export PYTHONPATH="$PYTHONPATH:$HOME/bluebild/pypeline/install"
+
+#python3 test.py                # Run test suite (optional, recommended)
+# python3 setup.py build_sphinx  # Generate documentation (optional)
