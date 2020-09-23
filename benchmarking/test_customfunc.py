911c8c1439d111a04057efd05d3caacd79e38696
etolley <eetolley@gmail.com>
2020-09-23 14:50:41 +0200

    updated test

--- benchmarking/test_customfunc.py
+++ benchmarking/test_customfunc.py
@@ -2,6 +2,7 @@ from numpy.ctypeslib import ndpointer
 from ctypes import *
 import numpy as np
 import sys
+import faulthandler
 
 
 ################################
@@ -42,39 +43,44 @@ sys.exit()
 
 '''
 
-def call_zgemm(M,N,K,A,B):
+def call_zgemm(M,N,K,A,B, a = 1, b = 0):
     # setting up C function
     so_file = "/home/etolley/bluebild/pypeline/src/zgemm-splat.so"
     custom_functions = CDLL(so_file)
     custom_functions.zgemm_pycall.argtypes=[c_int, c_int, c_int,
-                                    ndpointer(dtype=np.complex128,ndim=1,flags='C'),
+                                    ndpointer(dtype=np.complex128,ndim=1,flags='C'), #alpha
                                     ndpointer(dtype=np.complex128,ndim=2,flags='C'), c_int,
                                     ndpointer(dtype=np.complex128,ndim=2,flags='C'), c_int,
-                                    ndpointer(dtype=np.complex128,ndim=1,flags='C'),
+                                    ndpointer(dtype=np.complex128,ndim=1,flags='C'), # beta
                                     ndpointer(dtype=np.complex128,ndim=2,flags='C'), c_int]
     
     ldA = M
     ldB = K
     C = np.zeros( (M,N),dtype =np.complex128)
     ldC = M
-    beta =  make_complexdouble_array(1.)
-    alpha = make_complexdouble_array(1.)
+    alpha = make_complexdouble_array(a)
+    beta =  make_complexdouble_array(b)
+
 
     # function call
-    custom_functions.zgemm_pycall(M, N, K, alpha, A, ldA, B, ldB , beta, C, ldC)
+    faulthandler.enable()
+    custom_functions.zgemm_pycall(M, N, K, alpha, A, ldA, B, ldB, beta, C, ldC)
 
     return C
 
 # setting up inputs
-M = 15
-N = 15
+M = 8
+N = 8
 K = 3
 A = np.random.rand(M,K).astype(np.complex128)
 B = np.random.rand(K,N).astype(np.complex128)
 
-result_zgemm = call_zgemm(M,N,K,A,B)
+result_zgemm10 = call_zgemm(M,N,K,A,B, 1,0)
+result_zgemm11 = call_zgemm(M,N,K,A,B,1,1)
 result_matmul = np.matmul(A,B)
-print("First row from custom zgemm (M = {0}, N = {1}, K = {2}):".format(M,N,K))
-print(result_zgemm[0])
+print("First row from custom zgemm (M = {0}, N = {1}, K = {2}, alpha = 1, beta = 0):".format(M,N,K))
+print(result_zgemm10[0])
+print("First row from custom zgemm (M = {0}, N = {1}, K = {2}, alpha = 1, beta = 1):".format(M,N,K))
+print(result_zgemm11[0])
 print("First row from numpy matmul(M = {0}, N = {1}, K = {2}):".format(M,N,K))
 print(result_matmul[0])
