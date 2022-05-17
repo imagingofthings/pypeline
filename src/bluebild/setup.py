1c62ce9d8910c80728723230dc0b5b53ec6ab673
Simon Frasch <simon.frasch@cscs.ch>
2022-05-17 11:24:46 +0200

    Use PYBIND11 for python interface

--- src/bluebild/setup.py
+++ src/bluebild/setup.py
@@ -1,14 +1,22 @@
 from skbuild import setup
 import os
+import pathlib
+from setuptools import find_packages
+
+current_dir = pathlib.Path(__file__).parent.resolve()
+with open(str(current_dir) + '/VERSION') as f:
+    version = f.readline().strip()
 
 bluebild_gpu = str(os.getenv('BLUEBILD_GPU', 'CUDA'))
 
 setup(
     name="bluebild",
-    version="0.1.0",
+    version=version,
     description="Bluebild imaging algorithm",
     packages=['bluebild'],
-    package_dir={'': 'python'},
-    cmake_args=['-DBLUEBILD_GPU=' + bluebild_gpu, '-DBUILD_SHARED_LIBS=ON'],
+    package_dir={"": "python"},
+    cmake_install_dir="python", # must match package dir name. Otherwise, installed libraries are seen as independent data
+    include_package_data=True,
+    python_requires=">=3.6",
+    cmake_args=['-DBLUEBILD_GPU=' + bluebild_gpu, '-DBUILD_SHARED_LIBS=ON', '-DBLUEBILD_INSTALL=PYTHON'],
 )
-
--- src/bluebild/src/sensitivity_field_data.cpp
+++ src/bluebild/src/sensitivity_field_data.cpp
@@ -2,6 +2,7 @@
 #include <cstddef>
 
 #include "bluebild/bluebild.h"
+#include "bluebild/bluebild.hpp"
 #include "bluebild/config.h"
 #include "bluebild/context.hpp"
 #include "context_internal.hpp"
@@ -17,107 +18,125 @@
 
 namespace bluebild {
 
-template <typename T>
-auto sensitivity_field_data(ContextInternal& ctx, T wl, int m, int n, int nEig,
-                            const std::complex<T>* w, int ldw, const T* xyz, int ldxyz, T* d,
-                            std::complex<T>* v, int ldv) -> void {
-  if (ctx.processing_unit() == BLUEBILD_PU_GPU) {
+template <typename T, typename>
+BLUEBILD_EXPORT auto sensitivity_field_data(Context &ctx, T wl, int m, int n,
+                                            int nEig, const std::complex<T> *w,
+                                            int ldw, const T *xyz, int ldxyz,
+                                            T *d, std::complex<T> *v, int ldv)
+    -> void {
+  auto &ctxInternal = *InternalContextAccessor::get(ctx);
+  if (ctxInternal.processing_unit() == BLUEBILD_PU_GPU) {
 #if defined(BLUEBILD_CUDA) || defined(BLUEBILD_ROCM)
-        // Syncronize with default stream. TODO: replace with event
-        gpu::stream_synchronize(nullptr);
+    // Syncronize with default stream. TODO: replace with event
+    gpu::stream_synchronize(nullptr);
 
-        BufferType<gpu::ComplexType<T>> wBuffer, vBuffer;
-        BufferType<T> dBuffer, xyzBuffer;
-        auto wDevice = reinterpret_cast<const gpu::ComplexType<T>*>(w);
-        auto vDevice = reinterpret_cast<gpu::ComplexType<T>*>(v);
-        auto xyzDevice = xyz;
-        auto dDevice = d;
-        int ldwDevice = ldw;
-        int ldvDevice = ldv;
-        int ldxyzDevice = ldxyz;
+    BufferType<gpu::ComplexType<T>> wBuffer, vBuffer;
+    BufferType<T> dBuffer, xyzBuffer;
+    auto wDevice = reinterpret_cast<const gpu::ComplexType<T> *>(w);
+    auto vDevice = reinterpret_cast<gpu::ComplexType<T> *>(v);
+    auto xyzDevice = xyz;
+    auto dDevice = d;
+    int ldwDevice = ldw;
+    int ldvDevice = ldv;
+    int ldxyzDevice = ldxyz;
 
+    // copy input if required
+    if (!is_device_ptr(w)) {
+      wBuffer = create_buffer<gpu::ComplexType<T>>(
+          ctxInternal.allocators().gpu(), m * n);
+      ldwDevice = m;
+      wDevice = wBuffer.get();
+      gpu::check_status(gpu::memcpy_2d_async(
+          wBuffer.get(), m * sizeof(gpu::ComplexType<T>), w,
+          ldw * sizeof(gpu::ComplexType<T>), m * sizeof(gpu::ComplexType<T>), n,
+          gpu::flag::MemcpyDefault, ctxInternal.gpu_stream()));
+    }
 
-        // copy input if required
-        if(!is_device_ptr(w)) {
-          wBuffer = create_buffer<gpu::ComplexType<T>>(ctx.allocators().gpu(), m * n);
-          ldwDevice = m;
-          wDevice = wBuffer.get();
-          gpu::check_status(gpu::memcpy_2d_async(
-              wBuffer.get(), m * sizeof(gpu::ComplexType<T>), w, ldw * sizeof(gpu::ComplexType<T>),
-              m * sizeof(gpu::ComplexType<T>), n, gpu::flag::MemcpyDefault, ctx.gpu_stream()));
-        }
+    if (!is_device_ptr(xyz)) {
+      xyzBuffer = create_buffer<T>(ctxInternal.allocators().gpu(), 3 * m);
+      ldxyzDevice = m;
+      xyzDevice = xyzBuffer.get();
+      gpu::check_status(gpu::memcpy_2d_async(
+          xyzBuffer.get(), m * sizeof(T), xyz, ldxyz * sizeof(T), m * sizeof(T),
+          3, gpu::flag::MemcpyDefault, ctxInternal.gpu_stream()));
+    }
 
-        if(!is_device_ptr(xyz)) {
-          xyzBuffer = create_buffer<T>(ctx.allocators().gpu(), 3 * m);
-          ldxyzDevice = m;
-          xyzDevice = xyzBuffer.get();
-          gpu::check_status(gpu::memcpy_2d_async(xyzBuffer.get(), m * sizeof(T), xyz,
-                                                 ldxyz * sizeof(T), m * sizeof(T), 3,
-                                                 gpu::flag::MemcpyDefault, ctx.gpu_stream()));
-        }
+    // prepare output
+    if (!is_device_ptr(v)) {
+      vBuffer = create_buffer<gpu::ComplexType<T>>(
+          ctxInternal.allocators().gpu(), nEig * n);
+      ldvDevice = n;
+      vDevice = vBuffer.get();
+    }
+    if (!is_device_ptr(d)) {
+      dBuffer = create_buffer<T>(ctxInternal.allocators().gpu(), nEig);
+      dDevice = dBuffer.get();
+    }
 
-        // prepare output
-        if(!is_device_ptr(v)) {
-          vBuffer = create_buffer<gpu::ComplexType<T>>(ctx.allocators().gpu(), nEig * n);
-          ldvDevice = n;
-          vDevice = vBuffer.get();
-        }
-        if(!is_device_ptr(d)) {
-          dBuffer = create_buffer<T>(ctx.allocators().gpu(), nEig);
-          dDevice = dBuffer.get();
-        }
+    // call intensity_field_data on gpu
+    auto devInfo = sensitivity_field_data_gpu<T>(
+        ctxInternal, wl, m, n, nEig, wDevice, ldwDevice, xyzDevice, ldxyzDevice,
+        dDevice, vDevice, ldvDevice);
 
+    if (dBuffer) {
+      gpu::check_status(gpu::memcpy_async(d, dDevice, nEig * sizeof(T),
+                                          gpu::flag::MemcpyDeviceToHost,
+                                          ctxInternal.gpu_stream()));
+    }
+    if (vBuffer) {
+      gpu::check_status(gpu::memcpy_2d_async(
+          v, ldv * sizeof(gpu::ComplexType<T>), vBuffer.get(),
+          ldvDevice * sizeof(gpu::ComplexType<T>),
+          n * sizeof(gpu::ComplexType<T>), nEig, gpu::flag::MemcpyDeviceToHost,
+          ctxInternal.gpu_stream()));
+    }
 
-        // call intensity_field_data on gpu
-        auto devInfo =
-            sensitivity_field_data_gpu<T>(ctx, wl, m, n, nEig, wDevice, ldwDevice, xyzDevice,
-                                          ldxyzDevice, dDevice, vDevice, ldvDevice);
+    int hostInfo[2];
+    gpu::check_status(gpu::memcpy_async(
+        hostInfo, devInfo.get(), 2 * sizeof(int), gpu::flag::MemcpyDeviceToHost,
+        ctxInternal.gpu_stream()));
 
-        if(dBuffer) {
-          gpu::check_status(gpu::memcpy_async(d, dDevice, nEig * sizeof(T),
-                                              gpu::flag::MemcpyDeviceToHost, ctx.gpu_stream()));
-        }
-        if(vBuffer) {
-          gpu::check_status(gpu::memcpy_2d_async(
-              v, ldv * sizeof(gpu::ComplexType<T>), vBuffer.get(),
-              ldvDevice * sizeof(gpu::ComplexType<T>), n * sizeof(gpu::ComplexType<T>), nEig,
-              gpu::flag::MemcpyDeviceToHost, ctx.gpu_stream()));
-        }
+    // syncronize with stream to be synchronous with host
+    gpu::check_status(gpu::stream_synchronize(ctxInternal.gpu_stream()));
 
-        int hostInfo[2];
-        gpu::check_status(gpu::memcpy_async(hostInfo, devInfo.get(), 2 * sizeof(int),
-                                            gpu::flag::MemcpyDeviceToHost, ctx.gpu_stream()));
-
-        // syncronize with stream to be synchronous with host
-        gpu::check_status(gpu::stream_synchronize(ctx.gpu_stream())); 
-
-        // check if eigensolver threw error
-        if (hostInfo[0] || hostInfo[1]) {
-          throw EigensolverError();
-        }
+    // check if eigensolver threw error
+    if (hostInfo[0] || hostInfo[1]) {
+      throw EigensolverError();
+    }
 #else
-        throw GPUSupportError();
+    throw GPUSupportError();
 #endif
   } else {
-    sensitivity_field_data_host<T>(ctx, wl, m, n, nEig, w, ldw, xyz, ldxyz, d, v, ldv);
+    sensitivity_field_data_host<T>(ctxInternal, wl, m, n, nEig, w, ldw, xyz,
+                                   ldxyz, d, v, ldv);
   }
 }
 
+template auto sensitivity_field_data(Context &ctx, float wl, int m, int n,
+                                     int nEig, const std::complex<float> *w,
+                                     int ldw, const float *xyz, int ldxyz,
+                                     float *d, std::complex<float> *v, int ldv)
+    -> void;
+
+template auto sensitivity_field_data(Context &ctx, double wl, int m, int n,
+                                     int nEig, const std::complex<double> *w,
+                                     int ldw, const double *xyz, int ldxyz,
+                                     double *d, std::complex<double> *v,
+                                     int ldv) -> void;
+
 extern "C" {
-BLUEBILD_EXPORT BluebildError bluebild_sensitivity_field_data_s(BluebildContext ctx, float wl,
-                                                                int m, int n, int nEig, void* w,
-                                                                int ldw, const float* xyz,
-                                                                int ldxyz, float* d, void* v,
-                                                                int ldv) {
+BLUEBILD_EXPORT BluebildError bluebild_sensitivity_field_data_s(
+    BluebildContext ctx, float wl, int m, int n, int nEig, void *w, int ldw,
+    const float *xyz, int ldxyz, float *d, void *v, int ldv) {
   if (!ctx) {
     return BLUEBILD_INVALID_HANDLE_ERROR;
   }
   try {
     sensitivity_field_data<float>(
-        *InternalContextAccessor::get(*reinterpret_cast<Context *>(ctx)), wl, m,
-        n, nEig, reinterpret_cast<const std::complex<float> *>(w), ldw, xyz,
-        ldxyz, d, reinterpret_cast<std::complex<float> *>(v), ldv);
-  } catch (const bluebild::GenericError& e) {
+        *reinterpret_cast<Context *>(ctx), wl, m, n, nEig,
+        reinterpret_cast<const std::complex<float> *>(w), ldw, xyz, ldxyz, d,
+        reinterpret_cast<std::complex<float> *>(v), ldv);
+  } catch (const bluebild::GenericError &e) {
     return e.error_code();
   } catch (...) {
     return BLUEBILD_UNKNOWN_ERROR;
@@ -125,20 +144,18 @@ BLUEBILD_EXPORT BluebildError bluebild_sensitivity_field_data_s(BluebildContext 
   return BLUEBILD_SUCCESS;
 }
 
-BLUEBILD_EXPORT BluebildError bluebild_sensitivity_field_data_d(BluebildContext ctx, double wl,
-                                                                int m, int n, int nEig, void* w,
-                                                                int ldw, const double* xyz,
-                                                                int ldxyz, double* d, void* v,
-                                                                int ldv) {
+BLUEBILD_EXPORT BluebildError bluebild_sensitivity_field_data_d(
+    BluebildContext ctx, double wl, int m, int n, int nEig, void *w, int ldw,
+    const double *xyz, int ldxyz, double *d, void *v, int ldv) {
   if (!ctx) {
     return BLUEBILD_INVALID_HANDLE_ERROR;
   }
   try {
     sensitivity_field_data<double>(
-        *InternalContextAccessor::get(*reinterpret_cast<Context *>(ctx)), wl, m,
-        n, nEig, reinterpret_cast<const std::complex<double> *>(w), ldw, xyz,
-        ldxyz, d, reinterpret_cast<std::complex<double> *>(v), ldv);
-  } catch (const bluebild::GenericError& e) {
+        *reinterpret_cast<Context *>(ctx), wl, m, n, nEig,
+        reinterpret_cast<const std::complex<double> *>(w), ldw, xyz, ldxyz, d,
+        reinterpret_cast<std::complex<double> *>(v), ldv);
+  } catch (const bluebild::GenericError &e) {
     return e.error_code();
   } catch (...) {
     return BLUEBILD_UNKNOWN_ERROR;
@@ -147,4 +164,4 @@ BLUEBILD_EXPORT BluebildError bluebild_sensitivity_field_data_d(BluebildContext 
 }
 }
 
-}  // namespace bluebild
+} // namespace bluebild
