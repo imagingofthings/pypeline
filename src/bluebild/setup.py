b0af989e6441f0f6ef0da354d69f3cfc84f4f70f
Simon Frasch <simon.frasch@cscs.ch>
2022-05-31 15:49:50 +0200

    fix cmake args in setup.py

--- src/bluebild/setup.py
+++ src/bluebild/setup.py
@@ -9,6 +9,7 @@ with open(str(current_dir) + '/VERSION') as f:
 
 bluebild_gpu = str(os.getenv('BLUEBILD_GPU', 'CUDA'))
 bluebild_cmake_args = str(os.getenv('BLUEBILD_CMAKE_ARGS', ''))
+bluebild_cmake_args_list = [bluebild_cmake_args] if bluebild_cmake_args else []
 
 setup(
     name="bluebild",
@@ -19,6 +20,5 @@ setup(
     cmake_install_dir="python", # must match package dir name. Otherwise, installed libraries are seen as independent data
     include_package_data=True,
     python_requires=">=3.6",
-    cmake_args=['-DBLUEBILD_GPU=' + bluebild_gpu, '-DBUILD_SHARED_LIBS=ON', '-DBLUEBILD_INSTALL=PYTHON'] \
-            + [bluebild_cmake_args] if bluebild_cmake_args else []
+    cmake_args=['-DBLUEBILD_GPU=' + bluebild_gpu, '-DBUILD_SHARED_LIBS=ON', '-DBLUEBILD_INSTALL=PYTHON'] + bluebild_cmake_args_list
 )
