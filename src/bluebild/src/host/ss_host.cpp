de0ba4d7a6a1ba5434db0af0f084090ef1cef8b2
Etienne Orliac <etienne.orliac@epfl.ch>
2022-10-26 14:19:00 +0200

    Removing a useless function

--- src/bluebild/src/host/ss_host.cpp
+++ src/bluebild/src/host/ss_host.cpp
@@ -10,24 +10,12 @@ namespace bluebild {
 
 template <>
 SSHost<double>::SSHost(const size_t N, const double wl, const double* x) {
-
   ss_ = ssType();
 }
 
 template <>
-void SSHost<double>::execute(const int iter) {
-    printf("!!!!! do something here !!!!!\n");
-}
-
-template <>
 SSHost<float>::SSHost(const size_t N, const float wl, const float* x) {
-
     ss_ = ssType();
-}
-
-template <>
-void SSHost<float>::execute(const int iter) {
-    printf("!!!!! do something here 2 !!!!!\n");
 }
 
 } // namespace bluebild
